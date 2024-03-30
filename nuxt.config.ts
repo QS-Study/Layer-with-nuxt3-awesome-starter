9bea73102a67fd761bd626e28f780d9a88693afa
Lami <Halam.Lee.Lami@gmail.com>
2024-03-30 18:07:22 +0900

    feat: composables-layer, server-layer 사용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module'],
-  extends: ['@nuxt-awesome/theme'],
+  extends: [
+    '@nuxt-awesome/theme',
+    [
+      'github:QS-Study/Layer/layers/server-layer',
+      { auth: process.env.GITHUB_TOKEN },
+    ],
+    [
+      'github:QS-Study/Layer/layers/composables-layer',
+      { auth: process.env.GITHUB_TOKEN },
+    ],
+  ],
 });
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="component-wrapper">
+    <AwesomeButton to="/post" text="Post 페이지로 이동" />
+    <AwesomeButton
+      class="button"
+      text="Go Naver Button"
+      href="https://www.naver.com/" />
+    <AwesomeFormSwitch />
+    <div>{{ composableHello }}</div>
+    <div>{{ serverLayer }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const composableHello = useHello();
+const { data: serverLayer } = await useFetch('/api/hello');
+</script>
+
+<style scoped>
+.component-wrapper {
+  margin: 50px auto 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 500px;
+  height: 500px;
+}
+
+.button {
+  width: 200px;
+}
+</style>
